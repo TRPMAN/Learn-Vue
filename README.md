25500218835017a80cb2a31442e125aee31d1d12
TRPMAN <trp.man08@gmail.com>
2024-02-16 00:02:13 +0700

    EP Reaction Timer

---
+++ README.md
@@ -0,0 +1,16 @@
+# This is a Reaction-Timer Project
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
---
+++ src/components/Block.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="block" v-if="showBlock">
+        Click me
+    </div>
+</template>
+
+<script>
+export default {
+    props : ['delay'],
+    data(){
+        return{
+            showBlock : false
+        }
+    },
+
+    mounted() {
+        console.log("Component Mounted")
+        setTimeout(()=>{
+            this.showBlock = true
+            console.log(this.delay)
+        },this.delay)
+    },
+
+    updated(){
+        console.log("Component Update")
+    },
+
+    unmounted(){
+        console.log("unmounted")
+    }
+}
+</script>
+
+<style>
+    .block{
+        width: 500px;
+        border-radius: 20px;
+        background: green;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto
+    }
+</style>
+
